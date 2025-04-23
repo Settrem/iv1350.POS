20ab8a0ed4507e5c03247b23c3a94de2cc3126de
Settrem <adardeprem@gmail.com>
2025-04-23 14:34:38 +0200

    First Commit

---
+++ src/se/kth/iv1350/cashregister/integration/StorageRegistry.java
@@ -0,0 +1,5 @@
+package se.kth.iv1350.cashregister.integration;
+
+public class StorageRegistry {
+    
+}
---
+++ src/se/kth/iv1350/cashregister/startup/Main.java
@@ -0,0 +1,5 @@
+package se.kth.iv1350.cashregister.startup;
+
+public class Main {
+    
+}
