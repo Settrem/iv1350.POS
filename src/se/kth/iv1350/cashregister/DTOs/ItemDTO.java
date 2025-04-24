6983b4c3bc35e58cf68ac17a9a20c12c71added9
Razmus Nilsson <Razmus_n99@hotmail.com>
2025-04-24 22:50:41 +0200

    Created additem and getItem functions, Created constructor for ItemDTO

--- src/se/kth/iv1350/cashregister/DTOs/ItemDTO.java
+++ src/se/kth/iv1350/cashregister/DTOs/ItemDTO.java
@@ -1,27 +1,41 @@
 package se.kth.iv1350.cashregister.DTOs;
 
-public class ItemDTO {
+/**
+ * Contains information about an item
+ */
 
-    public int ItemID;
-
+public final class ItemDTO {
+    private int itemID;
+    private String name;
     private String description;
-    private int price;
-    private double vAT;
+    private double price;
+    private double vat;
 
-    
-
-    public double getVAT(){
-        return vAT;
+    public int getItemID() {
+        return itemID;
+    }
+/**
+ * Constructor for the ItemDTO class
+ */
+    public ItemDTO(int itemID, String name, String description, double price, double vat) {
+        this.itemID = itemID;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.vat = vat;
     }
 
-    public String getDescription(){
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
         return description;
     }
-
-    public int getPrice(){
+    public double getPrice() {
         return price;
     }
-
-
+    public double getVAT() {
+        return vat;
+    }
 
 }
--- src/se/kth/iv1350/cashregister/integration/ItemRegistry.java
+++ src/se/kth/iv1350/cashregister/integration/ItemRegistry.java
@@ -1,11 +1,44 @@
 package se.kth.iv1350.cashregister.integration;
 
 import se.kth.iv1350.cashregister.DTOs.ItemDTO;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemRegistry {
+    private List<ItemDTO> item = new ArrayList<>();
+
+    ItemRegistry() {
+        addItem();
+    }
+
+    /**
+     * Creates a new list to contain information about an item
+     * 
+     * @param itemID Items barcode
+     * @param name Name of item
+     * @param description The description of the item
+     * @param price Price of item
+     * @param vat Vat of item
+     */
+    private void addItem() {
+        item.add(new ItemDTO(123, "Milk", "refreshing!", 19.90, 6.0));
+        item.add(new ItemDTO(456, "Coke Zero", "This stuff is addicting!", 14.90, 6.0));
+        item.add(new ItemDTO(789, "Kexchoklad", "This should be illegal!", 12.90, 6.0));
+    }
+
+    /**
+     * 
+     * @param itemID Fetches item with scanned itemID
+     * @return Returns null if item is not find
+     */
+    public ItemDTO getItem(int itemID) {
+        for (ItemDTO item : item) {
+            if (item.getItemID() == itemID) {
+                return item;
+            }
+        }
+        return null; 
+    }
     
-
-
-
 
 }
