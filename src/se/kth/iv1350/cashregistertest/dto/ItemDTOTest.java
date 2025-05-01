013b5762e62118a7e21fca5863efd3db6b76dc39
Settrem <adardeprem@gmail.com>
2025-05-02 01:09:28 +0200

    removed unimportant imports in test files

--- src/se/kth/iv1350/cashregistertest/dto/ItemDTOTest.java
+++ src/se/kth/iv1350/cashregistertest/dto/ItemDTOTest.java
@@ -1,9 +1,9 @@
 package se.kth.iv1350.cashregistertest.dto;
 
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.*;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
--- src/se/kth/iv1350/cashregistertest/integration/PrinterTest.java
+++ src/se/kth/iv1350/cashregistertest/integration/PrinterTest.java
@@ -1,6 +1,5 @@
 package se.kth.iv1350.cashregistertest.integration;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import se.kth.iv1350.cashregister.integration.Printer;
 
